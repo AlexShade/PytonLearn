addc5d169fd015e31256946cad488eff33dbe953
lomantsev_al <lomantsev_al@open.ru>
2017-03-20 12:31:28 +0300

    add get bs4doc

--- PytonLearn/PytonLearn/BS4Utils.py
+++ PytonLearn/PytonLearn/BS4Utils.py
@@ -1,11 +1,20 @@
 from bs4 import BeautifulSoup
 
-#получить ссылки со страницы
+#get all links from bsObj
 def getAllLinks(bsObj, startWith = ""):
 	pages = list()
+	if bsObj is None:
+		return pages
 	for link in bsObj.findAll('a', href=True):
 		if "href" in link.attrs:
 			if link.attrs["href"] not in pages:
 				if (startWith =="") or (str(link.attrs["href"]).startswith(startWith)): #or (str(link.attrs["href"]).find(containText)>-1):
 					pages.append(link.attrs["href"])
 	return pages
+
+#get bs4 doc
+def getBS4Document(path):
+	errCode = 0
+	errText = ""
+
+	return errCode, errText, bs4Doc
